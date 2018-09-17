4480b32e6b38da69d7453f6a04db9b1a19c254b2
Julien Rebetez <julien@fhtagn.net>
2018-09-17 22:02:06 +0200

    Fix an issue when two triangle share 2 vertices that are on the plane.
      2
     /  \
    1 - -3
     \  /
       4
    
    In this situation, the previous implementation could start from triangle
    (1, 2, 3), visit vertex 1, use the triangle (1, 3, 4) as the next triangle
    and then visit vertex 3 and pick the initial triangle again, at which
    point it would stop thinking it has been around the mesh.
    
    This issue happen only when two triangles share two vertices that lie
    exactly on the plane (so both triangles have the same set of intersections
    with the plane).
    
    The solution is that once we have visited a triangle and chosed the next
    one from its neighbours, we remove all neighbouring triangles from the
    ‘to-visit’ list. This ensure we never go back to a previously visited
    triangle.
    
    Closes #7

---
+++ tests/test_intersection.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import os
+import sys
+import meshcut
+import numpy as np
+
+
+def test_triangle_plane_intersection_edge_edge():
+    verts = [
+        (0, 0, 0),
+        (0, 1, 0),
+        (1, 0, 0),
+    ]
+    faces = [
+        (0, 1, 2),
+    ]
+
+    mesh = meshcut.TriangleMesh(verts, faces)
+    plane = meshcut.Plane(
+        orig=(0, 0.5, 0),
+        normal=(0, 1, 0)
+    )
+
+    intersections = meshcut.compute_triangle_plane_intersections(
+        mesh, 0, plane)
+
+    assert len(intersections) == 2
+    assert intersections[0][0] == meshcut.INTERSECT_EDGE
+    assert np.allclose(intersections[0][1][1], 0.5)
+    assert intersections[1][0] == meshcut.INTERSECT_EDGE
+    assert np.allclose(intersections[1][1][1], 0.5)
+
+
+def test_triangle_plane_intersection_edge_on_plane():
+    verts = [
+        (0, 0, 0),
+        (0, 1, 0),
+        (1, 0, 0),
+    ]
+    faces = [
+        (0, 1, 2),
+    ]
+
+    mesh = meshcut.TriangleMesh(verts, faces)
+    plane = meshcut.Plane(
+        orig=(0, 0, 0),
+        normal=(0, 1, 0)
+    )
+
+    intersections = meshcut.compute_triangle_plane_intersections(
+        mesh, 0, plane)
+
+    assert len(intersections) == 2
